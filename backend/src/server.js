01451d89a80d4882d318e2369368d59c135af131
pavanaditya123 <Pavanadi88@gmail..com>
2025-12-22 14:00:16 +0530

    Fix frontend setup and restore package.json

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,27 +4,21 @@ import path from "path";
 
 const app = express();
 
-// Resolve absolute path
 const __dirname = path.resolve();
 
-// Health check
+
 app.get("/health", (req, res) => {
     res.status(200).json({ msg: "success from 1234" });
 });
 
-// Serve frontend in production
-if (ENV.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "frontend", "dist")));
-
-    // SPA fallback (VERY IMPORTANT)
-    app.get("*", (req, res) => {
-        res.sendFile(
-            path.join(__dirname, "frontend", "dist", "index.html")
-        );
+if (ENV.NODE_ENV == "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.get("/{*any}", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
     });
-}
 
-// Start server
-app.listen(ENV.PORT, () => {
-    console.log("server is running on the port", ENV.PORT);
-});
+}
+//make our app ready for deployment
+app.listen(ENV.PORT, () => console.log("server is running on the port", ENV.PORT));
+
+
